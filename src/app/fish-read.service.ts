004c3b7fbe208a1996302108bf3bacbb3d6a578f
juny524 <juny5241@gmail.com>
2021-11-22 23:50:26 +0900

    魚種データはJSONやめた

--- src/app/fish-read.service.ts
+++ src/app/fish-read.service.ts
@@ -1,20 +1,48 @@
 import { Injectable } from '@angular/core';
 import { FishInfo } from './fish-info';
+import * as fishJson from './json/fish.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FishReadService {
 
+  private fishdatas = [
+    {
+      "id": 100,
+      "name": "チヌ"
+    },
+    {
+      "id": 200,
+      "name": "グレ"
+    },
+    {
+      "id": 300,
+      "name": "シーバス"
+    },
+    {
+      "id": 400,
+      "name": "マダイ"
+    },
+    {
+      "id": 500,
+      "name": "青物"
+    },
+    {
+      "id": 600,
+      "name": "タチウオ"
+    }
+  ];
+
   constructor() { }
 
   // fish.jsonから魚種データを読み込む
   public read(): FishInfo[] {
 
-    var fishinfo: Array<FishInfo> = new Array(3);
-    fishinfo[0] = new FishInfo(100, "チヌ");
-    fishinfo[1] = new FishInfo(200, "グレ");
-    fishinfo[2] = new FishInfo(300, "シーバス");
+    var fishinfo: Array<FishInfo> = new Array(this.fishdatas.length);
+    this.fishdatas.forEach(fish => {
+      fishinfo.push(new FishInfo(fish.id, fish.name));
+    })
     return fishinfo; 
   }
   
